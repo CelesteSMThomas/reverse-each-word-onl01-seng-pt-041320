f525bb349aa2c320757317eb63202be7b0ab94b1
Celeste Thomas <celestesmthomas@gmail.com>
2020-04-22 16:26:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(array)
   new_array = []
-  array.each do |word|
-    new_array << ""
-  
-  
-  
+ array.each do |word|
+   new_array << word
+ end
+   new_array.join(" ")
+   new_array.reverse
 end
